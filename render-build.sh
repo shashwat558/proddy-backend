b66c0f73d7addf3ec7c3ce02461b31bd6f37fe09
shashwat558 <jainshashwat528@gmail.com>
2025-05-08 21:40:54 +0530

    Added render file

---
+++ render-build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+npm install
+npm run build  # Uncomment if you're using TypeScript or a build step
+
+# Puppeteer cache fix
+if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then 
+  echo "...Copying Puppeteer Cache from Build Cache" 
+  cp -R $XDG_CACHE_HOME/puppeteer/ $PUPPETEER_CACHE_DIR
+else 
+  echo "...Storing Puppeteer Cache in Build Cache" 
+  cp -R $PUPPETEER_CACHE_DIR $XDG_CACHE_HOME
+fi
